bc04adbb73aa9fb1f6a533aac7386e47f96533a0
Lee Winfield <winfieldlee01@gmail.com>
2021-04-03 22:26:16 -0400

    graphing a parabola with dynamic resolution

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,7 +5,20 @@ public class Graph : MonoBehaviour {
     [SerializeField]
     Transform pointPrefab = default;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
     void Awake () {
-        Instantiate(pointPrefab);
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        var position = Vector3.zero;
+        for (int i = 0; i < resolution; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
     }
 }
