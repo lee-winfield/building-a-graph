818f5005783baa1744b7e3cf9e8165bd7e9771fc
Lee Winfield <winfieldlee01@gmail.com>
2021-04-06 23:12:29 -0400

    functions are now in 3D

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -17,10 +17,15 @@ public class Graph : MonoBehaviour {
         float step = 2f / resolution;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++) {
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z++;
+            }
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -34,7 +39,7 @@ public class Graph : MonoBehaviour {
         for (int i = 0; i < points.Length; i++) {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
