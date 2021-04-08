578f105e61a6dfa68e31e337ec38eb8c8c79f40b
Lee Winfield <winfieldlee01@gmail.com>
2021-04-08 08:36:04 -0400

    rotating star

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -4,9 +4,9 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary {
     public delegate Vector3 Function (float u, float v, float t);
 
-    public enum FunctionName { Wave, MultiWave, Ripple, UVSphere, RotatingTwistingSphere };
+    public enum FunctionName { Wave, MultiWave, Ripple, UVSphere, RotatingTwistingSphere, Torus };
 
-    static Function[] functions = { Wave, MultiWave, Ripple, UVSphere, RotatingTwistingSphere };
+    static Function[] functions = { Wave, MultiWave, Ripple, UVSphere, RotatingTwistingSphere, Torus };
 
     public static Function GetFunction (FunctionName name) {
         return functions[(int)name];
@@ -56,6 +56,17 @@ public static class FunctionLibrary {
         Vector3 p;
         p.x = s * Sin(PI * u);
         p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 Torus (float u, float v, float t) {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
     }
