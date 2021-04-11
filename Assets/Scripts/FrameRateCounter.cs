4fb6548941557bf34b015251dade5cecdefa2dbd
Lee Winfield <winfieldlee01@gmail.com>
2021-04-11 14:26:31 -0400

    added a MS mode

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -6,7 +6,16 @@ public class FrameRateCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI display = default;
     [SerializeField, Range(0.1f, 2f)] private float sampleDuration = 1f;
+
+    private enum DisplayMode
+    {
+        FPS,
+        MS
+    }
+
+    [SerializeField] private DisplayMode _displayMode = DisplayMode.FPS;
     
+
     private int _frames;
     private float _duration, _bestDuration = float.MaxValue, _worstDuration;
     private void Update()
@@ -25,8 +34,28 @@ public class FrameRateCounter : MonoBehaviour
         }
 
         if (_duration < sampleDuration) return;
-        display.SetText("FPS\n{0:0}\n{0:0}\n{0:0}", _frames/_duration, 1f/_bestDuration, 1f/_worstDuration);
+        if (_displayMode == DisplayMode.FPS)
+        {
+            display.SetText(
+                "FPS\n{0:0}\n{1:0}\n{2:0}",
+                1f/_bestDuration,
+                _frames/_duration,
+                1f/_worstDuration
+                );
+           
+        }
+        else
+        {
+            display.SetText(
+                "MS\n{0:1}\n{1:1}\n{2:1}",
+                1000f * _bestDuration,
+                1000f * _duration / _frames,
+                1000f * _worstDuration
+                );
+        }
         _frames = 0;
         _duration = 0f;
+        _worstDuration = 0f;
+        _bestDuration = float.MaxValue;
     }
 }
