f02d3bc8cc3d8667de4263cc6b94b58e906decfa
Lee Winfield <winfieldlee01@gmail.com>
2021-04-11 13:55:33 -0400

    displaying best and worst frame rates

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -8,17 +8,25 @@ public class FrameRateCounter : MonoBehaviour
     [SerializeField, Range(0.1f, 2f)] private float sampleDuration = 1f;
     
     private int _frames;
-    private float _duration;
+    private float _duration, _bestDuration = float.MaxValue, _worstDuration;
     private void Update()
     {
         var frameDuration = Time.unscaledDeltaTime;
         _frames++;
         _duration += frameDuration;
-        if (_duration >= sampleDuration)
+        if (frameDuration < _bestDuration)
         {
-            display.SetText("FPS\n{0:0}\n000\n000", _frames / _duration);
-            _frames = 0;
-            _duration = 0f;
+            _bestDuration = frameDuration;
         }
+
+        if (frameDuration > _worstDuration)
+        {
+            _worstDuration = frameDuration;
+        }
+
+        if (_duration < sampleDuration) return;
+        display.SetText("FPS\n{0:0}\n{0:0}\n{0:0}", _frames/_duration, 1f/_bestDuration, 1f/_worstDuration);
+        _frames = 0;
+        _duration = 0f;
     }
 }
