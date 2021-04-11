fee7c8fcfe8e826491aaad3c781d73c9efe1d898
Lee Winfield <winfieldlee01@gmail.com>
2021-04-11 13:44:45 -0400

    using average framerate now

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class FrameRateCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI display = default;
-
+    [SerializeField, Range(0.1f, 2f)] private float sampleDuration = 1f;
+    
+    private int _frames;
+    private float _duration;
     private void Update()
     {
         var frameDuration = Time.unscaledDeltaTime;
-        display.SetText("FPS\n{0:0}\n000\n000", 1f / frameDuration);
+        _frames++;
+        _duration += frameDuration;
+        if (_duration >= sampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n000\n000", _frames / _duration);
+            _frames = 0;
+            _duration = 0f;
+        }
     }
 }
